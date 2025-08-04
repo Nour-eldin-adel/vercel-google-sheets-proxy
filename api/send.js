c81c872b95fdf8c67589d5dd1a0bc22aeaaa7692
Nour-eldin-adel <62nourel@gmail.com>
2025-08-04 06:35:12 +0300

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,4 +1,11 @@
 export default async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  return res.status(204).end(); // No Content
+}
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
